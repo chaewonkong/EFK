5c8587a04f8780a4bfa6597947b561607b0bae60
chaewonkong <chaewonkong@gmail.com>
2022-11-02 22:34:18 +0900

    chore: add zerolog

--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
 )
 
 func main() {
 	e := echo.New()
+	logger := zerolog.New(os.Stdout)
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			logger.Info().Str("URI", v.URI).Int("status", v.Status).Time("time", v.StartTime.Local()).Msg("Request")
+
+			return nil
+		},
+	}))
 
 	e.GET("/", handler)
-	e.Logger.Fatal(e.Start(":1323"))
+	logger.Fatal().Err(e.Start(":1323")).Msg("Server started")
 }
 
 func handler(c echo.Context) error {
