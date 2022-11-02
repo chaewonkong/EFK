57deb2f50c0c320395f464eec8f9e123c46527b3
chaewonkong <chaewonkong@gmail.com>
2022-11-02 22:10:16 +0900

    feat: add router

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", handler)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func handler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World!")
+}
